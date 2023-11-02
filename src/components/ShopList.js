1360ee221304e9fc0e8234e1895434a77df02ec3
Filip <Filip.Kwiatek@gmail.com>
2023-11-02 22:22:12 +0100

    Creating a new project

---
+++ src/components/ShopList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./ShopList.module.css";
+
+const ShopList = () => {
+  return (
+    <div className={classes.background}>
+      <h1>Shop List!</h1>
+      <p>xD</p>
+    </div>
+  );
+};
+
+export default ShopList;
