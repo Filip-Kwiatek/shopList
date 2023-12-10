a48b733229829f354d3bfb4b25e0d8860d22d5bf
Filip <Filip.Kwiatek@gmail.com>
2023-12-10 20:00:26 +0100

    Adding styles rendering dynamically and fixing how list is rendering

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -1,27 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./ShopList.module.css";
 
 const ShopList = () => {
+  const [form, setForm] = useState({
+    id: 0,
+    product: "",
+    quantity: "",
+    note: "",
+  });
+
+  const [shopListState, setShopListState] = useState([]);
+  const [disabledButton, setDisabledButton] = useState(false);
+
+  const addItem = () => {
+    setShopListState([
+      ...shopListState,
+      {
+        id: shopListState.length,
+        product: form.product,
+        quantity: form.quantity,
+        note: form.note,
+      },
+    ]);
+    return [...shopListState];
+  };
+
+  // const addNewItemAndResetForm = () => {
+  //   setShopListState((prevState) => {
+  //     return [form, ...prevState];
+  //   });
+  //   setForm({
+  //     product: "",
+  //     quantity: "",
+  //     note: "",
+  //   });
+  // };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setForm({
+      product: "",
+      quantity: "",
+      note: "",
+    });
+  };
+
   return (
     <div className={classes.wrapper}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <div className={classes.item}>
           <label className={classes.label}>Product</label>
-          <input className={classes.input} type="text" />
+          <input
+            className={classes.input}
+            type="text"
+            id="product"
+            onChange={(e) => setForm({ ...form, product: e.target.value })}
+            value={form.product}
+          />
         </div>
         <div className={classes.item}>
           <label className={classes.label}>Quantity</label>
-          <input className={classes.input} type="text" />
+          <input
+            className={classes.input}
+            type="text"
+            id="quantity"
+            onChange={(e) => setForm({ ...form, quantity: e.target.value })}
+            value={form.quantity}
+          />
         </div>
         <div className={classes.item}>
           <label className={classes.label}>Additional note</label>
-          <input className={classes.input} type="text" />
+          <input
+            className={classes.input}
+            type="text"
+            id="note"
+            onChange={(e) => setForm({ ...form, note: e.target.value })}
+            value={form.note}
+          />
         </div>
         <div className={classes.item}>
-          <button className={classes.button}>Add product</button>
+          <button
+            disabled={
+              !disabledButton
+                ? !form.product.length || !form.quantity.length
+                : null
+            }
+            style={{
+              backgroundColor:
+                !form.product.length || !form.quantity.length
+                  ? "rgb(84, 84, 84)"
+                  : "white",
+            }}
+            className={classes.button}
+            onClick={() => {
+              addItem();
+            }}
+          >
+            Add product
+          </button>
         </div>
       </form>
+      <ul className={classes.list}>
+        {shopListState.map((listItem, index) => {
+          return (
+            <>
+              <li key={listItem.id}>
+                {listItem.product} {listItem.quantity} {listItem.note}
+                <button
+                  className={classes.button}
+                  onClick={() => {
+                    setShopListState(
+                      shopListState.filter((item) => item.id !== listItem.id)
+                    );
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            </>
+          );
+        })}
+      </ul>
     </div>
   );
 };
